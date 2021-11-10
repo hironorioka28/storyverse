0ad154dd72a4ff6fcbb4dc689c06436f64a49f0c
hironorioka28 <hironorioka28@gmail.com>
2021-11-11 00:22:02 +0900

    Prepare environment

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Container, Heading, Text } from '@chakra-ui/react'
+const Home = (): JSX.Element => {
+  return (
+    <Container>
+      <Heading>Home</Heading>
+      <Text size="lg">Here is Home</Text>
+    </Container>
+  )
+}
+
+export default Home
